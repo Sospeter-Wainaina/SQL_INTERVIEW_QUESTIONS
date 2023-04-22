3199928d79df52706641313d6a9b7452686433e2
sospeter-wainaina <sospetergathungu2000@gmail.com>
2023-04-22 12:29:08 +0300

    update

--- Basic SQL concepts/Rank_only_Duplicates_Question.sql
+++ Basic SQL concepts/Rank_only_Duplicates_Question.sql
@@ -1,21 +1,22 @@
-create table list (id varchar(5));
-insert into list values ('a');
-insert into list values ('a');
-insert into list values ('b');
-insert into list values ('c');
-insert into list values ('c');
-insert into list values ('c');
-insert into list values ('d');
-insert into list values ('d');
-insert into list values ('e');
+--create table list (id varchar(5));
+--insert into list values ('a');
+--insert into list values ('a');
+--insert into list values ('b');
+--insert into list values ('c');
+--insert into list values ('c');
+--insert into list values ('c');
+--insert into list values ('d');
+--insert into list values ('d');
+--insert into list values ('e');
 
 with dup as (
-select *,
-row_number() over (partition by id order by id) as rnk
-from list)
-select 
-*,
-case when count(1) over (partition by id) > 1 then 'DUP' + cast(rnk as varchar)
-else NULL
-end as dupp
-from dup
+select id from list 
+group by id
+having count(1)>1
+),
+cte_rank as (select *,
+rank() over(order by id) as rnk from dup
+)
+
+select l.*,'DUP'+cast(cr.rnk as varchar) as con from list l
+left join cte_rank cr on cr.id=l.id
