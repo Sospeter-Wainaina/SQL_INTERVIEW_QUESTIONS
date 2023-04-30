702acd8bdd39f3e01161882cb72fb6af5c377450
sospeter-wainaina <sospetergathungu2000@gmail.com>
2023-04-30 20:02:03 +0300

    update comments

--- Advanced SQL Concepts/Winner_per_group.sql
+++ Advanced SQL Concepts/Winner_per_group.sql
@@ -1,7 +1,6 @@
 --create table players
 --(player_id int,
 --group_id int)
-
 --insert into players values (15,1);
 --insert into players values (25,1);
 --insert into players values (30,1);
@@ -11,7 +10,6 @@
 --insert into players values (50,2);
 --insert into players values (20,3);
 --insert into players values (40,3);
-
 --create table matches
 --(
 --match_id int,
@@ -19,24 +17,40 @@
 --second_player int,
 --first_score int,
 --second_score int)
-
 --insert into matches values (1,15,45,3,0);
 --insert into matches values (2,30,25,1,2);
 --insert into matches values (3,30,15,2,0);
 --insert into matches values (4,40,20,5,2);
 --insert into matches values (5,35,50,1,1);
 --select * from players
+--find winner, incase of a tie then the player with the lowest id wins
 with player_scores as(
-select first_player as player,first_score as score from matches
-union all
-select second_player as player,second_score as score from matches
-) ,
-final_scores as ( select group_id,player,sum(score) as final_score from player_scores p inner join players l on p.player = l.player_id 
-group by player,group_id
+    select first_player as player,
+        first_score as score
+    from matches
+    union all
+    select second_player as player,
+        second_score as score
+    from matches
 ),
-rnk as (select *,
-row_number() over(partition by group_id order by final_score desc,player asc) as num from final_scores
-
+final_scores as (
+    select group_id,
+        player,
+        sum(score) as final_score
+    from player_scores p
+        inner join players l on p.player = l.player_id
+    group by player,
+        group_id
+),
+rnk as (
+    select *,
+        row_number() over(
+            partition by group_id
+            order by final_score desc,
+                player asc
+        ) as num
+    from final_scores
 )
-select * from rnk
+select *
+from rnk
 where num = 1
