1110754ae08c835816382b1d256a8f5bc195a6c4
sospeter-wainaina <sospetergathungu2000@gmail.com>
2023-09-14 20:14:00 +0300

    PWC SQL interview question

--- Basic SQL concepts/Big4_SQL_Problem.sql
+++ Basic SQL concepts/Big4_SQL_Problem.sql
@@ -1,10 +1,23 @@
-create table company_revenue 
-(
-company varchar(100),
-year int,
-revenue int
-)
+--create table company_revenue 
+--(
+--company varchar(100),
+--year int,
+--revenue int
+--)
 
-insert into company_revenue values 
-('ABC1',2000,100),('ABC1',2001,110),('ABC1',2002,120),('ABC2',2000,100),('ABC2',2001,90),('ABC2',2002,120)
-,('ABC3',2000,500),('ABC3',2001,400),('ABC3',2002,600),('ABC3',2003,800);
+--insert into company_revenue values 
+--('ABC1',2000,100),('ABC1',2001,110),('ABC1',2002,120),('ABC2',2000,100),('ABC2',2001,90),('ABC2',2002,120)
+--,('ABC3',2000,500),('ABC3',2001,400),('ABC3',2002,600),('ABC3',2003,800);
+
+----Q1 Only output companies which have continously gained revenue else it should not output
+
+with CTE as (
+select *,
+isnull(lag(revenue) over(partition by company order by year ),0) as previous_revenue,
+CASE WHEN revenue>isnull(lag(revenue) over(partition by company order by year ),0) THEN 0 ELSE 1 END as diff
+
+from company_revenue)
+select COMPANY,COUNT(DIFF) AS PROFIT_YEARS
+from cte 
+GROUP BY COMPANY
+HAVING SUM(DIFF)=0
